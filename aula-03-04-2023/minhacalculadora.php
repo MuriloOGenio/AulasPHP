95f9363c44282bafb4a6e8d15eab6f1d6285d9f9
MuriloOGenio <mueugi@gmail.com>
2023-04-05 13:10:22 -0300

    aula-03-04-2023

---
+++ aula-03-04-2023/minhacalculadora.php
@@ -0,0 +1,59 @@
+<?php
+    function somarValor($n1,$n2){ 
+        $soma = $n1 + $n2;
+
+        return $soma;
+    }
+
+    function MultiValor($n1,$n2){ 
+        $multi = $n1 * $n2;
+    
+        return $multi;
+    }
+
+    function SubValor($n1,$n2){ 
+        $sub = $n1 - $n2;
+        
+        return $sub;
+        }
+    
+    function DivValor($n1, $n2){
+        $div = 0;
+        if($n2 == 0){
+            echo "Não se pode divir por zero. <br/>";
+
+        } elseif ($n1 == 0) {
+            echo "Não se pode dividir por zero.<br/>"
+
+        } 
+        else {
+            $div = $n1 / $n2;
+        }
+        return $div;
+    }
+
+    $n1 = 40;
+    $n2 = 20;
+    $operador = "+";
+
+    switch($operador){
+
+        case "+":
+            $resultado = somarValor($n1 , $n2);
+            break;
+
+        case "-":
+            $resultado = SubValor($n1 , $n2);
+            break;
+
+        case "*":
+            $resultado = MultiValor($n1 , $n2);
+            break;
+        
+        case "/":
+            $resultado = SubValor($n1 , $n2);
+
+
+    }
+
+?>
